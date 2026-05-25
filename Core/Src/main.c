7cd90cfb25935c51452362281d5bd17fd2b81d1f
Mirfa <151968120+mirfa2@users.noreply.github.com>
2026-05-26 01:07:06 +0200

    got PWM sine wave blue led working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,7 +43,9 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
+TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
+DMA_HandleTypeDef hdma_tim1_ch1;
 
 /* USER CODE BEGIN PV */
 
@@ -55,6 +57,7 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -63,10 +66,55 @@ static void MX_TIM2_Init(void);
 /* USER CODE BEGIN 0 */
 
 uint16_t ADC_VAL[2];	//store ADC data
+int DMAcount=0;
+int ADCcount=0;
+
+// Pre-computed 512-point sine wave scaled for ARR = 1000
+const uint16_t SINE_WAVE_LUT_512[512] = {
+     500,  506,  512,  518,  525,  531,  537,  543,  549,  555,  561,  567,  573,  579,  585,  592,
+     598,  604,  610,  616,  621,  627,  633,  639,  645,  651,  657,  663,  668,  674,  680,  686,
+     691,  697,  703,  708,  714,  719,  725,  730,  736,  741,  746,  752,  757,  762,  767,  773,
+     778,  783,  788,  793,  798,  803,  808,  812,  817,  822,  827,  831,  836,  840,  845,  849,
+     854,  858,  862,  866,  870,  875,  879,  883,  887,  890,  894,  898,  902,  905,  909,  912,
+     916,  919,  922,  926,  929,  932,  935,  938,  941,  944,  947,  949,  952,  955,  957,  960,
+     962,  964,  966,  969,  971,  973,  975,  977,  978,  980,  982,  983,  985,  986,  988,  989,
+     990,  992,  993,  994,  995,  995,  996,  997,  998,  998,  999,  999,  999, 1000, 1000, 1000,
+    1000, 1000, 1000, 1000,  999,  999,  999,  998,  998,  997,  996,  995,  995,  994,  993,  992,
+     990,  989,  988,  986,  985,  983,  982,  980,  978,  977,  975,  973,  971,  969,  966,  964,
+     962,  960,  957,  955,  952,  949,  947,  944,  941,  938,  935,  932,  929,  926,  922,  919,
+     916,  912,  909,  905,  902,  898,  894,  890,  887,  883,  879,  875,  870,  866,  862,  858,
+     854,  849,  845,  840,  836,  831,  827,  822,  817,  812,  808,  803,  798,  793,  788,  783,
+     778,  773,  767,  762,  757,  752,  746,  741,  736,  730,  725,  719,  714,  708,  703,  697,
+     691,  686,  680,  674,  668,  663,  657,  651,  645,  639,  633,  627,  621,  616,  610,  604,
+     598,  592,  585,  579,  573,  567,  561,  555,  549,  543,  537,  531,  525,  518,  512,  506,
+     500,  494,  488,  482,  475,  469,  463,  457,  451,  445,  439,  433,  427,  421,  415,  408,
+     402,  396,  390,  384,  379,  373,  367,  361,  355,  349,  343,  337,  332,  326,  320,  314,
+     309,  303,  297,  292,  286,  281,  275,  270,  264,  259,  254,  248,  243,  238,  233,  227,
+     222,  217,  212,  207,  202,  197,  192,  188,  183,  178,  173,  169,  164,  160,  155,  151,
+     146,  142,  138,  134,  130,  125,  121,  117,  113,  110,  106,  102,   98,   95,   91,   88,
+      84,   81,   78,   74,   71,   68,   65,   62,   59,   56,   53,   51,   48,   45,   43,   40,
+      38,   36,   34,   31,   29,   27,   25,   23,   22,   20,   18,   17,   15,   14,   12,   11,
+      10,    8,    7,    6,    5,    5,    4,    3,    2,    2,    1,    1,    1,    0,    0,    0,
+       0,    0,    0,    0,    1,    1,    1,    2,    2,    3,    4,    5,    5,    6,    7,    8,
+      10,   11,   12,   14,   15,   17,   18,   20,   22,   23,   25,   27,   29,   31,   34,   36,
+      38,   40,   43,   45,   48,   51,   53,   56,   59,   62,   65,   68,   71,   74,   78,   81,
+      84,   88,   91,   95,   98,  102,  106,  110,  113,  117,  121,  125,  130,  134,  138,  142,
+     146,  151,  155,  160,  164,  169,  173,  178,  183,  188,  192,  197,  202,  207,  212,  217,
+     222,  227,  233,  238,  243,  248,  254,  259,  264,  270,  275,  281,  286,  292,  297,  303,
+     309,  314,  320,  326,  332,  337,  343,  349,  355,  361,  367,  373,  379,  384,  390,  396,
+     402,  408,  415,  421,  427,  433,  439,  445,  451,  457,  463,  469,  475,  482,  488,  494
+};
+
+
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
 {
+	ADCcount++;
+}
 
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+{
+	DMAcount++;
 }
 
 
@@ -104,10 +152,14 @@ int main(void)
   MX_DMA_Init();
   MX_ADC1_Init();
   MX_TIM2_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
 
   HAL_TIM_Base_Start(&htim2);	//start timer2 as per configured
   HAL_ADC_Start_DMA(&hadc1, (uint32_t *)ADC_VAL, 2);	//start in adc in dma mode
+
+  //generate sine wave blue led f_sine = 100Hz/512 = 0.195Hz, tim1 freq = 100 Hz
+  HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)SINE_WAVE_LUT_512, 512);
 
   /* USER CODE END 2 */
 
@@ -239,6 +291,81 @@ static void MX_ADC1_Init(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 1800-1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 1000-1;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
   * @brief TIM2 Initialization Function
   * @param None
   * @retval None
@@ -252,7 +379,6 @@ static void MX_TIM2_Init(void)
 
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
 
   /* USER CODE BEGIN TIM2_Init 1 */
 
@@ -272,28 +398,15 @@ static void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
-  HAL_TIM_MspPostInit(&htim2);
 
 }
 
@@ -310,6 +423,9 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream0_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+  /* DMA2_Stream1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
 
 }
 
@@ -328,7 +444,6 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_10, GPIO_PIN_RESET);
